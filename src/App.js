8f4e3ea6ea931aadcd287bc653e4f06e823be85c
AlirezaEbadi <ebadia256@gmail.com>
2023-03-28 18:14:15 +0430

    all project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./App.css"
+import MainEmail from './containers/MainEmail/MainEmail'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+    <MainEmail/>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
